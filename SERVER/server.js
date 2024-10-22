e9b1bd6d4d3160387e89072b48f65ac48b0dff8b
HanumaraddiBV <hanumaraddin14@gmail.com>
2024-10-23 00:28:41 +0530

    added markedown edit component and cors, marked package installed

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,8 +1,29 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { marked } from "marked";
+
+
 
 const app = express();
 
 
+app.use(cors());
+app.use(bodyParser.json());
+
+//converting markdown to HTML 
+app.post('/convert', (req, res) => {
+    const { markdown } = req.body;
+    if (!markdown) {
+      return res.status(400).json({ error: 'Markdown input is required.' });
+    }
+    
+    // Converting markdown to HTML
+    const html = marked(markdown);
+    return res.json({ html });
+  });
+
+  
 //port setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
